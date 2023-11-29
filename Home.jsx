b430002aaf32658f9666cebd6311be98ee2a99cd
diogo-benchimol <146952584+diogo-benchimol@users.noreply.github.com>
2023-11-29 13:47:19 -0300

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => { 
+
+return ( 
+    <> 
+      <h1 style={{color:"#171616", textAlign:"center"}}>  
+          Bem vindo a Valt</h1> 
+    </> 
+) 
+}; 
+
+export default Home;
+
